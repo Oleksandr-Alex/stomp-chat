7a5efff57f5c13344df595f238898ec55c9f8517
Oleksandr Bandurka <oleksandr.alex.b@gmail.com>
2021-05-04 00:35:57 +0300

    Removed Google verification file

--- src/main/java/com/alexb/chat/config/SecurityConfig.java
+++ src/main/java/com/alexb/chat/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.alexb.chat.config;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -10,15 +11,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable().antMatcher("/**").authorizeRequests()
-                    .antMatchers("/", "/index", "/google1c329871f85132d3.html").authenticated()
+                    .antMatchers("/", "/index").authenticated()
                     .anyRequest().authenticated()
                     .and()
                     .oauth2Login().permitAll()
                     .and()
                     .logout()
                     .invalidateHttpSession(true)
-                    .clearAuthentication(true)
-                    .logoutSuccessUrl("/");
+                    .clearAuthentication(true);
     }
 
 }
