f98b6bebb03fe1ec269d84f20ee479593a35bef9
Oleksandr Bandurka <oleksandr.alex.b@gmail.com>
2021-05-04 00:13:16 +0300

    Added Google verification file

--- src/main/java/com/alexb/chat/config/SecurityConfig.java
+++ src/main/java/com/alexb/chat/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.alexb.chat.config;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -11,7 +10,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable().antMatcher("/**").authorizeRequests()
-                    .antMatchers("/", "/index").authenticated()
+                    .antMatchers("/", "/index", "/google1c329871f85132d3.html").authenticated()
                     .anyRequest().authenticated()
                     .and()
                     .oauth2Login().permitAll()
